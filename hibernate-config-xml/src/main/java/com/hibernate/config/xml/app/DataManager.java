b065524df04d78ec9ebbcfa827561cb238c68de7
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2022-02-02 23:00:22 +0530

    Code Modified based on hibernate 5.0
    Code Modified based on hibernate 5.0

--- hibernate-config-xml/src/main/java/com/hibernate/config/xml/app/DataManager.java
+++ hibernate-config-xml/src/main/java/com/hibernate/config/xml/app/DataManager.java
@@ -3,19 +3,27 @@ package com.hibernate.config.xml.app;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 import com.hibernate.config.xml.model.Student;
 
 public class DataManager {
 	public static void main(String[] args) {
 
-		Configuration configuration = new Configuration();
-		configuration.configure("hibernate.cfg.xml");
-		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-				.applySettings(configuration.getProperties());
-		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		SessionFactory sessionFactory = null;
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+		        .configure() // configures settings from hibernate.cfg.xml
+		        .build();
+		try {
+		    sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		}
+		catch (Exception e) {
+		    // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+		    // so destroy it manually.
+		    StandardServiceRegistryBuilder.destroy( registry );
+		}
 		Session session = sessionFactory.openSession();
 		Transaction transaction = session.beginTransaction();
 		System.out.println("start");
