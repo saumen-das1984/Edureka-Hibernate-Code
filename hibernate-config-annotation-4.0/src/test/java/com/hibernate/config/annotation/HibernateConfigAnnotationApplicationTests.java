137dbd4c420ff237e3098147250a65fbcaefef86
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2022-02-03 20:29:31 +0530

    Hibernate using Annotation based on hibernate 4.0
    Hibernate Mapping using Annotation based on hibernate 4.0

---
+++ hibernate-config-annotation-4.0/src/test/java/com/hibernate/config/annotation/HibernateConfigAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.config.annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateConfigAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
